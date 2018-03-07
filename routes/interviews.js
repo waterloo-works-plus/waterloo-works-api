8f70ff9d4d2828ea0a124d9353e463389e919e2c
Munaz Rahman <m64rahma@uwaterloo.ca>
2018-03-06 18:01:12 -0800

    Fix code

--- routes/interviews.js
+++ routes/interviews.js
@@ -32,7 +32,7 @@ module.exports = app => {
       
       try {
         const postingsApplicationsButtonIdentifier = '#displayStudentMyPostingsApplications';
-        await page.waitForSelector(postingsApplicationsButtonIdentifier, { timeout: 30000 });
+        await page.waitForSelector(postingsApplicationsButtonIdentifier, { timeout: 10000 });
       } catch (error) {
         return res.json({
           status: 'Error',
@@ -40,16 +40,9 @@ module.exports = app => {
         });
       }
         
-      try{
+      try {
         await page.evaluate(() => {
-            orbisApp.buildForm({
-                    'action':'_-_-PxVYq5SPbB7o0JJRl0Gh8BdugSNZd64ge_d1WJxK3AtOgXgImo-LEUJNug-NK65EY0HMRX9i8bpxHQiSPkaiVGqQLOhvSmfgLLVmcNWIrPJ3mU9OzC5A3LWxJseVkJXh0419h-UU1woQ6wdXOmcmuQbeciQwjf4AM4zBjWUWlju1A38',
-                    'numOfDays':'-1',
-                    'selectedFilter':''
-                }, 
-                '/myAccount/co-op/interviews-co-op.htm', 
-                ''
-            ).submit();
+          orbisApp.buildForm({'action':'_-_-RpQB0w5b3x0Y2uBJbh_dK7tQ3LxZhUGWd2WGW7IvIpjec3bVMnW4PIemg5LIFm2nvbpVeNxDXUopX34BdV3ROoHMycmqkMcPcjAJdwkAvh-EnLy-RTJcXq-GypjlVMlewztx7PxHHuAyknqkOzDfLMWQyimxUYNjOhY2hOh9j96ICfE','numOfDays':'-1','selectedFilter':'booked'}, '/myAccount/co-op/interviews-co-op.htm', '').submit();
         });  
       } catch (error) {
         return res.json({
@@ -61,12 +54,12 @@ module.exports = app => {
       
       try {
         const interviewsTableIdentifier = '#ccrm_studentInterviewsTableID';
-        await page.waitForSelector(interviewsTableIdentifier, { visible: true, timeout: 30000 });
+        await page.waitForSelector(interviewsTableIdentifier, { visible: true, timeout: 10000 });
       } catch (error) {
         // Tables didn't populate 
-        // Let's check for  No records message 
+        // Let's check for no records message 
         const noRecordsMessageIdentifier = '#ccrm_studentInterviews_noRecordsMessage';
-        await page.waitForSelector(noRecordsMessageIdentifier, { visible: true, timeout: 30000 });
+        await page.waitForSelector(noRecordsMessageIdentifier, { visible: true, timeout: 1000 });
 
         // No records message must be visible
         return res.json({
@@ -80,12 +73,12 @@ module.exports = app => {
       
       const numInterviews = parseInt($('.badge-info').text());
       const interviewRows = $('tr').slice(1, numInterviews);
-      const interviews = [];
+      const interviewIds = [];
+      let interviews = {};
 
       for (let i = 0; i < interviewRows.length; i++) {
         const interviewRow = $(interviewRows[i]);
-        const interviewCells = interviewRows.find('td');
-        
+        const interviewCells = interviewRow.find('td');
 
         const termCell = $(interviewCells[1]);
         const scheduleStatusCell  = $(interviewCells[2]);
@@ -100,26 +93,33 @@ module.exports = app => {
         const divisionCell = $(interviewCells[11]);
         const mockInterviewCell = $(interviewCells[12]);
 
-        interviews.push({
+        // Generate an interview id based on the job id and time
+        const interviewDateTime = Date.parse(dateTimeCell.text() + ' EST');
+        const jobId = jobIdCell.text();
+        const interviewId = jobId.toString() + interviewDateTime.valueOf().toString();
+
+        interviewIds.push(interviewId);
+        interviews[interviewId] = {
           term: termCell.text(),
           scheduleStatus: scheduleStatusCell.text(),
           confirmationStatus: confirmationStatusCell.text(),
-          dateTime: Date.parse(dateTimeCell.text() + ' ' + 'EST'),
+          dateTime: interviewDateTime,
           type: typeCell.text(),
           location: locationCell.text(),
           method: methodCell.text(),
-          jobId: parseInt(jobIdCell.text()),
+          jobId: jobId,
           jobTitle: jobTitleCell.text(),
           organization: organizationCell.text(),
           division: divisionCell.text(),
           mockInterview: (mockInterviewCell.text() === 'No' ? false : true),
-        });
+        };
       }
 
       await browser.close();
       return res.json({
         status: 'OK',
-        data: interviews,
+        interviews: interviews,
+        interviewIds: interviewIds,
       });
     } catch (error) {
       return res.json({
