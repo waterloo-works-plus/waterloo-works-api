af973be89677f745414f391e8ce0f412d73b2b1c
Munaz Rahman <m64rahma@uwaterloo.ca>
2018-03-06 18:55:17 -0800

    Refactor logic

--- routes/applications.js
+++ routes/applications.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+
+const authLib = require('../lib/authLib');
+const applicationLib = require('../lib/applicationsLib');
 
 module.exports = app => {
 
@@ -23,96 +25,17 @@ module.exports = app => {
     try {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto('https://cas.uwaterloo.ca/cas/login?service=https://waterlooworks.uwaterloo.ca/waterloo.htm');
-      
-      const usernameIdentifier = '#username';
-      await page.waitForSelector(usernameIdentifier);
-      await page.type(usernameIdentifier, username);
-    
-      const passwordIdentifier = '#password';
-      await page.waitForSelector(passwordIdentifier);
-      await page.type(passwordIdentifier, password);
-    
-      const submitButtonIdentifier = '#cas-submit';
-      await page.waitForSelector(submitButtonIdentifier);
-      await page.click(submitButtonIdentifier);
     
       try {
-        const postingsApplicationsButtonIdentifier = '#displayStudentMyPostingsApplications';
-        await page.waitForSelector(postingsApplicationsButtonIdentifier, { timeout: 10000 });
+        await authLib.login(page, username, password);
       } catch (error) {
         return res.json({
           status: 'Error',
           message: 'Failed to login'
         });
       }
-    
-      await page.evaluate(selectedTerm => {
-        orbisApp.buildForm({'action':'_-_-OmTSRZVRaEMr9pw1r9-auRDZ7vo0565eaAf9pNzLAKslFQpz1cW0GjaRD5MBgffmGrbIgRl-8GGIG06aRwWkyykAbNt2OCjT8G8baBI30tp3AxDGpiiGRlI9NksFAZwNbC3QNLF-xlrvGhvmN0SxgcXiubkp2IP9wsWUHLK_qbLFfV_dnbZylzA6zlZBri8','numOfDays':'-1','selectedTerm':selectedTerm}, '/myAccount/co-op/coopApplications.htm', '').submit();
-      }, selectedTerm);
-    
-      try {
-        const applicationsTableIdentifier = '#na_studentApplicationGridTableID';
-        await page.waitForSelector(applicationsTableIdentifier, { visible: true, timeout: 10000 });
-      } catch (error) {
-        // Tables didn't populate
-        // Let's check if no applications were found
-        const noRecordsMessageIdentifier = '#na_studentApplicationGrid_noRecordsMessage';
-        await page.waitForSelector(noRecordsMessageIdentifier, { visible: true, timeout: 10000 });
 
-        // No applications message must be visible
-        return res.json({
-          status: 'OK',
-          jobIds: [],
-          jobs: {},
-        });
-      }
-      
-      const content = await page.content();
-      const $ = cheerio.load(content);
-    
-      const numApps = parseInt($('.badge-info').text());
-      const appRows = $('tr').slice(1, numApps);
-      const jobIds = [];
-      let jobs = {};
-
-      for (let i = 0; i < appRows.length; i++) {
-        const appRow = $(appRows[i]);
-        const appCells = appRow.find('td');
-
-        const termCell = $(appCells[1]);
-        const jobIdCell = $(appCells[2]);
-        const titleCell = $(appCells[3]);
-        const companyCell = $(appCells[4]);
-        const divisionCell = $(appCells[5]);
-        const locationCell = $(appCells[6]);
-        const cityCell = $(appCells[7]);
-        const appStatusCell = $(appCells[9]);
-        const jobStatusCell = $(appCells[10]);
-        const openingsCell = $(appCells[11]);
-        const appDeadlineCell = $(appCells[12]);
-        const appSubmittedOnCell = $(appCells[13]);
-        const appSubmittedByCell = $(appCells[14]);
-
-        const jobId = jobIdCell.text();
-        jobIds.push(jobId);
-
-        jobs[jobId] = {
-          term: termCell.text(),
-          jobId: jobId,
-          title: titleCell.text(),
-          company: companyCell.text(),
-          division: divisionCell.text(),
-          location: locationCell.text(),
-          city: cityCell.text(),
-          appStatus: appStatusCell.text(),
-          jobStatus: jobStatusCell.text(),
-          openings: parseInt(openingsCell.text()),
-          appDeadline: appDeadlineCell.text(),
-          appSubmittedOn: appSubmittedOnCell.text(),
-          appSubmittedBy: appSubmittedByCell.text(),
-        };
-      }
+      const { jobIds, jobs } = await applicationLib.getApplications(page, selectedTerm);
 
       await browser.close();
       return res.json({
@@ -124,7 +47,7 @@ module.exports = app => {
       return res.json({
         status: 'Error',
         message: 'An unknown error occurred',
-        error: error,
+        error: error.message,
       });
     }
   });
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
+const authLib = require('../lib/authLib');
+
 module.exports = app => {
 
   app.post('/auth/login', async (req, res) => {
@@ -16,29 +18,14 @@ module.exports = app => {
     try {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-      await page.goto('https://cas.uwaterloo.ca/cas/login?service=https://waterlooworks.uwaterloo.ca/waterloo.htm');
       
-      const usernameIdentifier = '#username';
-      await page.waitForSelector(usernameIdentifier);
-      await page.type(usernameIdentifier, username);
-    
-      const passwordIdentifier = '#password';
-      await page.waitForSelector(passwordIdentifier);
-      await page.type(passwordIdentifier, password);
-    
-      const submitButtonIdentifier = '#cas-submit';
-      await page.waitForSelector(submitButtonIdentifier);
-      await page.click(submitButtonIdentifier);
-    
-      const postingsApplicationsButtonIdentifier = '#displayStudentMyPostingsApplications';
-      await page.waitForSelector(postingsApplicationsButtonIdentifier, { timeout: 10000 });
+      await authLib.login(page, username, password);
 
       await browser.close();
       return res.json({
         status: 'OK',
         message: 'Login successful',
       });
-
     } catch (error) {
       return res.json({
         status: 'Error',
@@ -48,4 +35,4 @@ module.exports = app => {
     }
   });
 
-}
+};
