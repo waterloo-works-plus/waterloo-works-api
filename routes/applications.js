9d16b37119a10edf89822c24387b36d2850a510a
Munaz Rahman <m64rahma@uwaterloo.ca>
2018-03-06 21:43:03 -0800

    Add get job route

--- routes/applications.js
+++ routes/applications.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 const authLib = require('../lib/authLib');
-const applicationLib = require('../lib/applicationsLib');
+const applicationsLib = require('../lib/applicationsLib');
 
 module.exports = (app) => {
   app.post('/applications/get', async (req, res) => {
@@ -43,13 +43,59 @@ module.exports = (app) => {
       const {
         jobIds,
         jobs,
-      } = await applicationLib.getApplications(page, selectedTerm);
+      } = await applicationsLib.getApplications(page, selectedTerm);
 
       await browser.close();
       return res.json({
         status: 'OK',
         jobIds: jobIds,
         jobs: jobs,
+      });
+    } catch (error) {
+      return res.json({
+        status: 'Error',
+        message: 'An unknown error occurred',
+        error: error.message,
+      });
+    }
+  });
+
+  app.post('/applications/getJob', async (req, res) => {
+    const {
+      username,
+      password,
+      jobId,
+      selectedTerm,
+    } = req.body;
+
+    if (!username || !password || !jobId || !selectedTerm) {
+      return res.json({
+        status: 'Error',
+        message: 'Missing parameter username, password, jobId, selectedTerm',
+      });
+    }
+
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      try {
+        await authLib.login(page, username, password);
+      } catch (error) {
+        return res.json({
+          status: 'Error',
+          message: 'Failed to login',
+        });
+      }
+
+      const {
+        job,
+      } = await applicationsLib.getApplicationById(page, selectedTerm, jobId);
+
+      await browser.close();
+      return res.json({
+        status: 'OK',
+        job: job,
       });
     } catch (error) {
       return res.json({
