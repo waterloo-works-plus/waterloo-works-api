8aca77a0dc3de1b5db200a844b9fe4f93f051a81
Munaz Rahman <m64rahma@uwaterloo.ca>
2018-03-28 20:59:39 -0700

    Add get interview endpoint

--- routes/interviews.js
+++ routes/interviews.js
@@ -17,7 +17,7 @@ module.exports = (app) => {
     if (!username || !password) {
       return res.json({
         status: 'Error',
-        message: 'Missing parameter username, password, numOfDays',
+        message: 'Missing parameter username, password',
       });
     }
 
@@ -48,6 +48,60 @@ module.exports = (app) => {
         status: 'OK',
         interviews: interviews,
         interviewIds: interviewIds,
+      });
+    } catch (error) {
+      return res.json({
+        status: 'Error',
+        message: 'An unknown error occurred',
+        error: error.message,
+      });
+    } finally {
+      await browser.close();
+    }
+  });
+
+  app.post('/interviews/get-interview', async (req, res) => {
+    const {
+      username,
+      password,
+    } = req.body;
+    let {
+      numOfDays,
+      interviewId,
+    } = req.body;
+
+    if (!username || !password || !interviewId) {
+      return res.json({
+        status: 'Error',
+        message: 'Missing parameter username, password, interviewId',
+      });
+    }
+
+    if (!numOfDays) {
+      numOfDays = -1;
+    }
+
+    const browser = await puppeteer.launch(puppeteerUtil.getLaunchFlags());
+
+    try {
+      const page = await browser.newPage();
+
+      try {
+        await authLib.login(page, username, password);
+      } catch (error) {
+        return res.json({
+          status: 'Error',
+          message: 'Failed to login',
+        });
+      }
+
+      const {
+        interview,
+      } = await interviewsLib.getInterview(page, interviewId, numOfDays);
+
+      return res.json({
+        status: 'OK',
+        interview: interview,
       });
     } catch (error) {
       return res.json({
