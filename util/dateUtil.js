e00294b902dc4e18b129b3a0de3d42cdaba5ccb3
MunazR <munaz.rahman@uwaterloo.ca>
2018-05-28 15:51:14 -0400

    Fix linter issues

--- util/dateUtil.js
+++ util/dateUtil.js
@@ -1,8 +1,9 @@
 module.exports = {
     isDST: () => {
       const now = new Date();
-      return (now.getMonth() > 4 || (now.getMonth() === 4 && now.getDate() >= 11)) &&
-        (now.getMonth() < 10 || (now.getMonth() === 10 && now.getDate() <= 4));
+      return (now.getMonth() > 4 ||
+        (now.getMonth() === 4 && now.getDate() >= 11)) &&
+        (now.getMonth() < 10 ||
+        (now.getMonth() === 10 && now.getDate() <= 4));
     },
   };
-  
